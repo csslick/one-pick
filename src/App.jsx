ddc00dcb33d5d6083cfe301b6f100a6b87cdff94
csslick <digitone@naver.com>
2023-08-18 01:12:34 +0900

    결과화면

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,67 @@ import { useState, useEffect } from 'react'
 import './App.scss'
 
 function App() {
-  const data = ['임영웅', '아이유', '싸이', '이찬원', '정동원', '강다니엘', '김호중'];
-  const [country, setCountry] = useState(data); // 전체 데이터
+  const data = [
+    { name: '임영웅', image: '/images/iyw.jpg' },
+    { name: '아이유', image: '/images/iu.jpg' },
+    { name: '싸이', image: '/images/psy.jpg' },
+    { name: '이찬원', image: '/images/leechan.jpg' },
+    { name: '정동원', image: '/images/jdw.jpg' },
+    { name: '강다니엘', image: '/images/kd.jpg' },
+    { name: '김호중', image: '/images/khj.jpg' },
+  ];
+  const [star, setStar] = useState(data); // 전체 데이터
   const [pick, setPick] = useState([]); // 선택 변수(2개 중 택)
 
   // 1쌍의 선택 요소 가져오기
   const getPair = () => {
-    let item1 = country[0];
-    let item2 = country[1];
+    let item1 = star[0];
+    let item2 = star[1];
     setPick([item1, item2]);
   }
 
-  // 선택하지 않은 요소 제거
+  // 제거 함수
   const removeItem = (id) => {
     console.log(id);
-    const updateItem = country.filter((item, i) => {
+    const updateItem = star.filter((item, i) => {
       return i !== id;
     })
-    setCountry(updateItem); // 제거한 배열 업데이트
+    setStar(updateItem); // 제거한 배열 업데이트
     console.log(updateItem);
   }
 
   useEffect(() => {
-    if(country.length > 1) getPair();
-  }, [country]); // country 배열이 변경되면 나머지 한쌍을 다시 불러옴
+    getPair();
+  }, [star]); // country 배열이 변경되면 나머지 한쌍을 다시 불러옴
 
   return (
     <div className="App">
       <h1>One Pick</h1>
       <div className="choice-box">
-        <button onClick={() => { removeItem(1)}}>{pick[0]}</button>
         {
-          country.length > 1 &&
-          <button onClick={() => { removeItem(0)}}>{pick[1]}</button>
+          pick.length > 0 &&
+          <button
+            style={{ backgroundImage: `url(${pick[0].image})` }}
+            onClick={() => { removeItem(1) }}>{pick[0].name}
+          </button>
+        }
+        {
+          pick.length > 0 && star.length > 1 ?
+            <button
+              style={{ backgroundImage: `url(${pick[1].image})` }}
+              onClick={() => { removeItem(0) }}>{pick[1].name}
+            </button> : <button></button>
         }
       </div>
+      { star.length === 1 && 
+        <>
+          <button 
+            className='btn-restart' 
+            onClick={() => {location.reload()}}
+          >재시작</button> 
+          <p>당신은 {pick[0].name}을 가장 좋아합니다!</p>
+        </>  
+      } 
     </div>
   )
 }
